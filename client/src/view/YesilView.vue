878ae952894e514e44776b2729465a803aa7ee08
sapazutku <64533036+sapazutku@users.noreply.github.com>
2022-05-06 14:04:22 +0300

    update YesilView

--- client/src/view/YesilView.vue
+++ client/src/view/YesilView.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="container">
     <h1>Yeşil Alanlar</h1>
-    <YesilMap :visiblePopUp="visiblePopUp"/>
-    <br>
-    <br>
-    <!-- TODO: etkinlikleri çek etkinliklerden parkidsi olanlara buton ekle  -->
+    <YesilMap :visiblePopUp="visiblePopUp" ref="childComponent" />
+    <br />
+    <br />
     <div>
-      <h2>Etkinlikler</h2>
-      <div v-for="(activePark,index) in activeParks" :key="index">
-        <button >{{ activePark.parkId }}</button>
+      
+      <div class="container is-max-desktop">
+        <h2 class="title">Etkinlikler</h2>
+        <div class="activity" v-for="(activePark, index) in activeParks" :key="index">
+          <span id="title">{{activePark.title}}</span>
+          <span id="location">📍 {{activePark.parkName}}</span>
+          <b-button id="btn-find" type="is-success is-light" @click="sendSearch(activePark.parkName)">
+            <span>Bul</span>
+          </b-button>
+        </div>
       </div>
     </div>
   </div>
@@ -16,27 +22,34 @@
 
 <script>
 import axios from "axios";
-import YesilMap from '../components/YesilMap.vue';
+import YesilMap from "../components/YesilMap.vue";
 export default {
   data() {
     return {
       visiblePopUp: "hidden",
       activeParks: [],
+      willSearch: "",
     };
   },
   components: {
-    YesilMap
+    YesilMap,
   },
   methods: {
-    
+    sendSearch(name) {
+      this.willSearch = name;
+      this.$emit("takeSearch", name);
+      console.log(this.willSearch);
+      this.$refs.childComponent.getData(name);
+    },
   },
-  mounted:function() {
-      axios.get("http://localhost:5000/posts/").then((response) => {
-        console.log(response.data);
-        this.activeParks = response.data
-        //this.parklar = response.data;
-      });
-    }
+  // etkinlik olan parklar
+  mounted: function () {
+    axios.get("http://localhost:5000/posts/").then((response) => {
+      console.log(response.data);
+      this.activeParks = response.data;
+      //this.parklar = response.data;
+    });
+  },
 };
 </script>
 
@@ -47,7 +60,25 @@ export default {
 .popup {
   visibility: hidden;
 }
-.container{
+.container {
   width: 70%;
+}
+.activity {
+  display: flex;
+  border:1px solid black;
+  margin: 5%;
+}
+#title{
+  width: 40%;
+  margin: 5%;
+
+}
+#location{
+  width: 40%;
+
+}
+#btn-find{
+  width: 10%;
+  margin: 5%;
 }
 </style>
